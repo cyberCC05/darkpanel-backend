edb7516b4bdce1db984fc2511e9015564a8e4a76
cyberCC05 <darken_ae@gmail.com>
2025-11-12 18:42:06 +0500

    dfdssdf

--- api/config.js
+++ api/config.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
   return res.status(200).json({
-    apiBase:
-      process.env.API_BASE || 'https://darkpanel-backend-swart.vercel.app/api',
+    apiBase: `${
+      req.headers.origin || 'https://darkpanel-backend-swart.vercel.app'
+    }/api`,
     github:
       process.env.GITHUB_RAW ||
       'https://raw.githubusercontent.com/Cyber05CC/darkpanel/main',
